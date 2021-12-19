f36c8baae281a6295d5419ebb2f8ce45b9c37ecf
Marcus Barbu <mgb.barbu@gmail.com>
2021-12-18 16:17:03 -0800

    first 18 start

---
+++ day18/a.py
@@ -0,0 +1,273 @@
+import math
+from dataclasses import dataclass, field
+from typing import Any, Optional
+from ppbtree import *
+
+def pt(node):
+    return print_tree(node, nameattr='label', left_child='left', right_child='right')
+
+@dataclass
+class Node:
+    label: str
+    rawlist: Optional[Any] = None
+    val: Optional[int] = 0
+    isleaf: Optional[bool] = False
+    left: Optional[Any] = None
+    right: Optional[Any] = None
+    parent: Optional[Any] = None
+
+    def __repr__(self) -> str:
+        return f'{self.val}' if self.isleaf else f'[{self.left},{self.right}]'
+    
+    def is_pair(self):
+        return (self.left is not None) and (self.right is not None) and self.left.isleaf and self.right.isleaf
+
+class SnailTree:
+    def __init__(self, linestr, label=''):
+        self.raw = linestr
+        self.root = Node(rawlist=eval(self.raw), isleaf=False, parent=None, label="root{}".format(label))
+        self.parse_node()
+    
+    def add(self, r_root):
+        left = self.root
+        right = r_root.root
+        self.root = Node(isleaf=False, parent=None, label='root')
+        left.parent = self.root
+        right.parent = self.root
+        self.root.left = left
+        self.root.right = right
+
+    def parse_node(self, root=None):
+        if root is None:
+            root = self.root
+        if not root.isleaf and (root.left is None and root.right is None):
+            line = root.rawlist
+            if isinstance(line, int):
+                root.val = line
+                root.isleaf = True
+                root.label = str(root.val)
+            elif isinstance(line, list):
+                root.val = None
+                root.isleaf = False
+                root.left = Node(rawlist=line[0], parent=root, label="l")
+                root.right = Node(rawlist=line[1], parent=root, label='r')
+        if root.left is not None:
+            self.parse_node(root=root.left)
+        if root.right is not None:
+            self.parse_node(root=root.right)
+    
+    def node_to_out(self, root=None):
+        if root is None:
+            root = self.root
+        output = []
+        output.append('[')
+
+    
+    def find_left_uncle(self, node):
+        parent = node.parent
+        prev = node
+        while parent is not None:
+            if parent.left == prev: # We're on the left side, need to go higher
+                prev = parent
+                parent = parent.parent
+            else:
+                return parent.left
+        return None
+    
+    def find_right_aunt(self, node):
+        parent = node.parent
+        prev = node
+        while parent is not None:
+            if parent.right == prev: # We're on the right side, need to go higher
+                prev = parent
+                parent = parent.parent
+            else:
+                return parent.right
+        return None
+
+    def find_leftmost_leaf(self, node):
+        while not node.isleaf and node:
+            if node.left is not None:
+                node = node.left
+            else:
+                return node.right
+        return node
+
+    def find_rightmost_leaf(self, node):
+        while not node.isleaf and node:
+            if node.right is not None:
+                node = node.right
+            else:
+                return node.left
+        return node
+
+    def find_next_left_leaf(self, node):
+        current = node
+        if current == self.root:
+            return None
+        if not current:
+            return None
+        parent = node.parent
+        if parent.left is not None and node is not parent.left: #easieset case, node is in the same chain
+            if parent.left.isleaf:
+                return parent.left
+            else: #walk the neighboring node down the right hand side
+                parent = parent.left
+                current = parent.right
+                while current:
+                    if current.isleaf:
+                        return current
+                    current = current.right
+        else:
+            return self.find_next_left_leaf(parent)
+
+    def find_next_right_leaf(self, node):
+        current = node
+        if current == self.root:
+            return None
+        if not current:
+            return None
+        parent = node.parent
+        if parent.right is not None and node is not parent.right: #easieset case, node is in the same chain
+            if parent.right.isleaf:
+                return parent.right
+            else: #walk the neighboring node down the left hand side
+                parent = parent.right
+                current = parent.left
+                while current:
+                    if current.isleaf:
+                        return current
+                    current = current.left
+        else:
+            return self.find_next_right_leaf(parent)
+
+        
+    def find_explosions_once(self, node=None, depth=0):
+        if node is None:
+            node = self.root
+
+        if node.left is not None:
+            if self.find_explosions_once(node.left, depth+1):
+                return True
+
+        if depth >= 4 and node.is_pair(): # if we're a pair at depth 4 or greater
+
+            left_leaf = self.find_next_left_leaf(node)
+            right_leaf = self.find_next_right_leaf(node)
+
+            # left_node = self.find_left_uncle(node) # find the left node that's above us
+            # if left_node and left_node is not node: #if it exists, find it's right most leaf to add to
+            #     leaf = self.find_rightmost_leaf(left_node)
+            #     if leaf is node:
+            #         print('failing because identical node thingy left')
+            #     if leaf is not None and leaf is not node:
+            #         leaf.val += node.left.val
+            #         leaf.label = str(leaf.val)
+
+            # right_node = self.find_right_aunt(node)
+            # if right_node and right_node is not node:
+            #     leaf = self.find_leftmost_leaf(right_node)
+            #     if leaf is node:
+            #         print('failing because identical node thingy right')
+            #     if leaf is not None and leaf is not node:
+            #         leaf.val += node.right.val
+            #         leaf.label = str(leaf.val)
+
+            # turn this node into a 0 leaf
+            if left_leaf:
+                left_leaf.val += node.left.val
+                left_leaf.label = str(left_leaf.val)
+            if right_leaf:
+                right_leaf.val += node.right.val
+                right_leaf.label = str(right_leaf.val)
+            node.isleaf = True
+            node.val = 0
+            node.label = '0'
+            node.right = None
+            node.left = None
+
+            return True
+        if node.right is not None:
+            if self.find_explosions_once(node.right, depth+1):
+                return True
+        return False
+    
+    def find_splits_once(self, node=None):
+        if node is None:
+            node = self.root
+        if node.isleaf and node.val >= 10:
+            lval = math.floor(node.val / 2)
+            rval = math.ceil(node.val / 2)
+            node.isleaf = False
+            node.label = f'was{node.val}'
+            node.val = 0
+            node.left = Node(rawlist=lval, label=str(lval), isleaf=True, val=lval, parent=node)
+            node.right = Node(rawlist=rval, label=str(rval), isleaf=True, val=rval, parent=node)
+            return True
+        if node.left is not None:
+            if self.find_splits_once(node.left):
+                return True
+        if node.right is not None:
+            if self.find_splits_once(node.right):
+                return True
+        return False
+    
+    def find_all_reductions(self, dbg=False):
+        # pt(self.root)
+        i = 0
+        exploded = False
+        split = False
+        running = True
+        while running:
+            exploded = self.find_explosions_once()
+            if exploded:
+                print(f"exploded: {self.root}")
+            if not exploded:
+                split = self.find_splits_once()
+                print(f"splitted: {self.root}")
+            running = exploded or split
+            if dbg:
+                pt(self.root)
+
+    def get_magnitude(self, node=None):
+        if node is None:
+            node = self.root
+        if node.isleaf:
+            return node.val
+        total = 0
+        if node.left:
+            total += 3 * self.get_magnitude(node.left)
+        if node.right:
+            total += 2 * self.get_magnitude(node.right)
+        return total
+
+
+test_input = '''[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]
+[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]'''
+
+test_input = '''[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+[7,[5,[[3,8],[1,4]]]]
+[[2,[2,2]],[8,[8,1]]]
+[2,9]
+[1,[[[9,3],9],[[9,0],[0,7]]]]
+[[[5,[7,4]],7],1]
+[[[[4,2],2],6],[8,7]]'''
+
+lines = test_input.splitlines()
+lines = open('input','r').read().splitlines()
+root = SnailTree(lines[0], label='orig')
+i = 1
+for l in lines[1:]:
+    snb = SnailTree(l, label=str(i))
+    root.add(snb)
+    root.find_all_reductions()
+    print(f'After step {i}' + '=' * 20)
+    pt(root.root)
+    print(root.root)
+    i += 1
+
+print(root.get_magnitude())
+print(root.root)
